8183978ea21539455fd89b5190b839f9513df47e
lkalicki <lkalicki98@gmail.com>
2021-12-24 17:00:58 -0500

    added voter routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+
+module.exports = router;
+
+router.use(require('./partyRoutes'));
+
+router.use(require('./voterRoutes'));
+
+
